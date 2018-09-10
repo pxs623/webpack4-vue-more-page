69e7433b09756c8ff6df0b64d1d992a98924a6bb
xuesong peng <328890391@qq.com>
2018-09-10 22:07:40 +0800

    Update server.js

--- webpack4-vue-more-page-cli/server.js
+++ webpack4-vue-more-page-cli/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 
-app.use(express.static('src/pages'));
+app.use(express.static('dist'));
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
